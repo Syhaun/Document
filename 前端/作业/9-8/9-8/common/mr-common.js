2922153accce0c6b2121d213e0839aa4361d9757
Syhaun <2776299759@qq.com>
2024-06-27 16:38:35 +0800

    updtae

--- 前端/作业/9-8/9-8/common/mr-common.js
+++
@@ -1,9 +0,0 @@
-function closeCode(){
-	var codeArea=document.getElementById("codeArea");
-    codeArea.className="container containerHome";
-}
-
-function showCode(){
-    var codeArea=document.getElementById("codeArea");
-    codeArea.className="container containerHome viewerOpen";
-}
